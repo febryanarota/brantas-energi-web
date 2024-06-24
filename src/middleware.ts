9e18605b3ba673c3f99d57f3af8fe0e99786ea5c
febryanarota <arota0202@gmail.com>
2024-06-24 13:47:45 +0700

    [FEAT] add auth and middleware

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { updateSession } from "@/lib/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+    const response = await updateSession(request);
+  
+    // If updateSession returned a response, return it
+    if (response) {
+        return response;
+    }
+
+    // Otherwise, continue with the request
+    return NextResponse.next();
+}
