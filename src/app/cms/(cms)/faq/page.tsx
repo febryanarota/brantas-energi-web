33558153cee4fbc7c4be6b93e6099ca6e315f5e3
febryanarota <arota0202@gmail.com>
2024-06-26 08:40:39 +0700

    [FIX] CMS: QNA page

---
+++ src/app/cms/(cms)/faq/page.tsx
@@ -0,0 +1,14 @@
+import { CMSContainer } from "@/components/ui/container";
+
+export default function Page() {
+    return (
+        <div>
+            <CMSContainer heading="Frequently Asked Questions">
+                <div>
+                    test
+                </div>
+            </CMSContainer>
+        </div>
+    )
+    
+};
