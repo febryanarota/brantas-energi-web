48a87b44502904302a8822cc21ca92cf40fb7cb4
febryanarota <arota0202@gmail.com>
2024-06-27 11:32:10 +0700

    [FIX] CMS: faq page

--- src/app/api/faq/route.ts
+++ src/app/api/faq/route.ts
@@ -3,14 +3,42 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
-    const result = await prisma.qna.findMany();
-    console.log(result);
+    let result;
+    const sessionExists = req.headers.get('cookie')?.valueOf();
+    
+    // Get the search parameters from the request URL
+    const searchParams = new URL(req.url).searchParams;
+    const statusParam = searchParams.get('status')?.toLowerCase();
+
+    // Determine query based on session and status parameter
+    if (statusParam === 'all' && sessionExists) {
+      result = await prisma.qna.findMany({
+        orderBy: {
+          position: 'asc'
+        }
+      }); // Access all when session exists
+    } else {
+      result = await prisma.qna.findMany({
+        where: {
+          status: "verified"
+        },
+        select: {
+          question: true,
+          answer: true,
+          position: true
+        },
+        orderBy: {
+          position: 'asc'
+        }
+      }); 
+    }
+
     return NextResponse.json(result);
   } catch (error) {
     console.error("Error fetching data:", error);
     return NextResponse.json(
       { error: "Internal Server Error" },
-      { status: 500 },
+      { status: 500 }
     );
   }
 }
--- src/app/cms/(cms)/faq/page.tsx
+++ src/app/cms/(cms)/faq/page.tsx
@@ -1,12 +1,47 @@
-"use client";
 import DraggableList from "@/components/draggableList/draggableList";
 import { CMSContainer } from "@/components/ui/container";
+import { getSession } from "@/lib/auth";
+import { qna } from "@prisma/client";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+// import { useEffect, useState } from "react";
 
-export default function Page() {
+export async function getData(): Promise<qna[]> {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/faq?status=all`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Cookie' : cookies().get('session')?.value || ''
+      },
+      credentials: 'include'
+    });
+    
+    if (!res.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const result : qna[] = await res.json();
+    return result
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return []; // Return an empty array in case of error
+  }
+}
+
+export default async function Page() {
+
+  const session = await getSession();
+  if (!session) redirect('/cms/login');
+
+  const data : qna[] = await getData()
+  
+
   return (
     <div>
       <CMSContainer heading="Frequently Asked Questions">
-        <DraggableList />
+        {/* TO DO: pass contentBlocks not the typeBlocks and its data */}
+        <DraggableList type="faq" data={data}/>
       </CMSContainer>
     </div>
   );
