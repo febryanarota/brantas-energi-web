666ed909fbd4461b32bf675d09578a29bf07eac4
febryanarota <arota0202@gmail.com>
2024-07-01 14:01:10 +0700

    [FIX] cookie request

--- src/app/(public)/frequently-asked-questions/page.tsx
+++ src/app/(public)/frequently-asked-questions/page.tsx
@@ -1,19 +1,21 @@
 import { qna } from "@prisma/client";
 import Faq from "./faq";
+import { cookies } from "next/headers";
 
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/faq`, {
     method : 'GET',
-    headers : {
-      'Content-Type' : 'application/json',
+    headers: {
+      'Content-Type': 'application/json',
+      'Cookie' : cookies().get('session')?.value || ''
     },
-    credentials : 'include'
+    credentials: 'include'
   });
 
   const data : qna[] = await res.json();
-  console.log(data);
+  console.log(data)
   return (
     <Faq data={data} />
   )
