40b77138794d92f479bc9ccc6f6ac4063c2e94aa
febryanarota <arota0202@gmail.com>
2024-07-01 08:34:20 +0700

    [FEAT] CMS: profile

--- src/app/cms/(cms)/profile/page.tsx
+++ src/app/cms/(cms)/profile/page.tsx
@@ -1,9 +1,48 @@
+import DraggableList from "@/components/draggableList/draggableList";
+import { CMSContainer } from "@/components/ui/container";
 import { getSession } from "@/lib/auth";
+import { profile } from "@prisma/client";
+import { Plus } from "lucide-react";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default async function page() {
-  const session = await getSession();
-  // if (!session) redirect('/cms/login');
+async function getData(): Promise<profile[]> {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/profile`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Cookie' : cookies().get('session')?.value || ''
+      },
+      credentials: 'include'
+    });
 
-  return <div>profile page</div>;
+    if (!res.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const result : profile[] = await res.json();
+    return result
+  } catch (error) {
+    console.log(error)
+    return [];
+  }
+}
+
+
+export default async function Page() {
+  const session = await getSession();
+  if (!session) redirect('/cms/login');
+
+  const data : profile[] = await getData();
+  return (
+    <div>
+      <CMSContainer>
+        <div className="flex flex-row justify-between items-center border-b-3 pb-2">
+          <h1 className="text-3xl font-bold tracking-widerfle">Company Profile</h1>
+        </div>
+        <p>{data[0].facebook}</p>
+      </CMSContainer>
+    </div>
+  );
 }
