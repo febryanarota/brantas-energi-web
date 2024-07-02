3f50998fcea6d914b7fabd3fd3ccf06bfad17e43
febryanarota <arota0202@gmail.com>
2024-07-02 16:56:51 +0700

    [FEAT] GET API text

--- src/app/api/text/route.ts
+++ src/app/api/text/route.ts
@@ -1,6 +1,24 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+export async function GET(req: NextRequest) {
+  const sessionExists = req.cookies.get("session");
+  if (!sessionExists) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  try {
+    const result = await prisma.text.findMany();
+    return NextResponse.json(result);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+
+}
+
 export async function POST(req: NextRequest) {
   const sessionExists = req.cookies.get("session");
 
