7508ede34ab6f55cb858371b31fdfd5084666193
febryanarota <arota0202@gmail.com>
2024-07-03 10:11:13 +0700

    [FEAT] integrate regulasi cms page with API

--- src/app/cms/(cms)/regulasi/page.tsx
+++ src/app/cms/(cms)/regulasi/page.tsx
@@ -1,15 +1,59 @@
 import { CMSContainer } from "@/components/ui/container";
 import FormTrigger from "@/components/forms/forms";
+import DraggableList from "@/components/draggableList/draggableList";
+import { contentBlock } from "@prisma/client";
+import { cookies } from "next/headers";
+import { getSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+
+async function getData(): Promise<contentBlock[]> {
+  const sessionCookie = cookies().get("session")?.value || "";
+  console.log("Session Cookie:", sessionCookie);
+
+  try {
+    // TO DO: retrive from content table
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_URL}/api/content?page=regulasi&status=all`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Cookie": `session=${sessionCookie}`,
+        },
+        credentials: "include",
+      },
+    );
+
+    console.log("Response status:", res.status);
+
+    if (!res.ok) {
+      const errorResponse = await res.text();
+      console.error("API Response Error:", errorResponse);
+      throw new Error(`Network response was not ok: ${res.status} ${res.statusText}`);
+    }
+
+    const result: contentBlock[] = await res.json();
+    return result;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return []; // Return an empty array in case of error
+  }
+}
+
+export default async function Page() {
+  const session = await getSession();
+  if (!session) redirect("/cms/login");
+
+  const data: contentBlock[] = await getData();
   return (
     <div>
       <CMSContainer>
         <div className="flex flex-row justify-between items-center border-b-3 pb-2">
           <h1 className="text-3xl font-bold tracking-widerfle">Regulasi</h1>
-          <FormTrigger />
-          {/* the button supposed to be here */}
+          <FormTrigger page={"regulasi"} session={session}/>
         </div>
+        <DraggableList data={data} />
       </CMSContainer>
     </div>
   );
