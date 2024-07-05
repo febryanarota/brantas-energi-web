3f6b65a11c2889cad8d7ca0d8487af56cb33863d
febryanarota <arota0202@gmail.com>
2024-07-05 10:37:38 +0700

    [FEAT] PATCH page API

--- src/app/api/page/[id]/route.ts
+++ src/app/api/page/[id]/route.ts
@@ -55,3 +55,58 @@ export async function PUT(
     );
   }
 }
+
+export async function PATCH(
+  req: NextRequest,
+  context: { params: { id: string } },
+) {
+  const body = await req.json();
+  const sessionExists = req.cookies.get("session");
+
+  if (!sessionExists) {
+    return NextResponse.json(
+      { error: "Unauthorized" },
+      { status: 401 },
+    );
+  }
+
+  try {
+    // Fetch the current positions
+    const currentData = await prisma.page.findUnique({
+      where: {
+        id: context.params.id,
+      },
+      select: {
+        positions: true,
+      },
+    });
+
+    if (!currentData) {
+      return NextResponse.json(
+        { error: "Page not found" },
+        { status: 404 },
+      );
+    }
+
+    // Concatenate the current positions with the new ones
+    const newPositions = [...currentData.positions, ...body.positions];
+
+    // Update the page with the new positions array
+    const result = await prisma.page.update({
+      where: {
+        id: context.params.id,
+      },
+      data: {
+        positions: newPositions,
+      },
+    });
+
+    return NextResponse.json(result);
+  } catch (error) {
+    console.error("Error updating data:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
