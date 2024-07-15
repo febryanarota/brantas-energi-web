9086cddb6543337f959d16f12a7f5fb966021d5d
febryanarota <arota0202@gmail.com>
2024-07-15 14:33:48 +0700

    [FIX] file API

--- src/app/api/file/[id]/route.ts
+++ src/app/api/file/[id]/route.ts
@@ -42,11 +42,11 @@ export async function DELETE(
 
     if (result.isFile) {
       if (result.link) {
-        const file = "public/" + result.link.split('/').pop();
+        const file = "public/" + result.link.split("/").pop();
         await storage.file.delete(file as string).catch((error) => {
           console.error("Error deleting file:", error);
           throw new Error("Error deleting file");
-        })
+        });
       } else {
         throw new Error("Link not found");
       }
@@ -76,39 +76,74 @@ export async function PUT(
   const role = session.role;
   const body = await req.formData();
   const blockId = parseInt(body.get("blockId") as string, 10);
-  const file = body.get("file") as File;
-  const fileExtension = file.name.split(".").pop();
-  const uuid = shortUUID.generate();
-
+  const display = body.get("display") as string;
+  const newIsFile =
+    body.get("isFile")?.toString().toLowerCase() === "true" ? true : false;
 
   try {
     const id = parseInt(context.params.id, 10);
-    const fileName = await prisma.image.findFirst({
+    const fileTarget = await prisma.file.findFirst({
       where: {
         id: id,
       },
     });
-    
-    const deleteImage = "public/" + fileName?.shadowId.split('/').pop();
-    await storage.image.delete(deleteImage).catch((error) => {
-      console.error("Error deleting image:", error);
-      throw new Error("Error deleting image");
-    })
 
-    const result = await prisma.image.update({
+    const oldIsFile = fileTarget?.isFile;
+
+    if (oldIsFile) {
+      // remove the file in storage
+      const deleteFile = "public/" + fileTarget?.link?.split("/").pop();
+      await storage.file.delete(deleteFile).catch((error) => {
+        console.error("Error deleting file:", error);
+        throw new Error("Error deleting file");
+      });
+    }
+
+    let link;
+    if (newIsFile) {
+      const file = body.get("file") as File;
+      const fileExtension = file.name.split(".").pop();
+      const uuid = shortUUID.generate();
+
+      await storage.file
+        .upload(`/public/${uuid}.${fileExtension}`, file)
+        .catch((error) => {
+          console.error("Error uploading image:", error);
+          throw new Error("Error uploading image");
+        });
+
+      link = `/public/${uuid}.${fileExtension}`;
+      // if (role !== "admin") {
+      //   const res = await prisma.contentBlock.update({
+      //     where: {
+      //       id: blockId,
+      //     },
+      //     data: {
+      //       status: "updatePending",
+      //     },
+      //   });
+
+      //   if (!res) {
+      //     return NextResponse.json(
+      //       { error: "Failed to update content block status" },
+      //       { status: 500 },
+      //     );
+      //   }
+      // }
+      // return NextResponse.json(result);
+    } else {
+      link = body.get("link") as string;
+    }
+
+    const result = await prisma.file.update({
       where: {
         id: id,
       },
       data: {
-        shadowId : `/public/${uuid}.${fileExtension}`,
-        alt: body.get("alt") as string,
+        link: link,
+        display: display,
+        isFile: newIsFile,
       },
-    });
-
-
-    await storage.image.upload(`/public/${uuid}.${fileExtension}`, file).catch((error) => {
-      console.error("Error uploading image:", error);
-      throw new Error("Error uploading image");
     });
 
     if (role !== "admin") {
@@ -129,6 +164,12 @@ export async function PUT(
       }
     }
 
+    if (!result) {
+      return NextResponse.json(
+        { error: "Failed to update file" },
+        { status: 500 },
+      );
+    }
     return NextResponse.json(result);
   } catch (error) {
     console.error("Error updating data:", error);
