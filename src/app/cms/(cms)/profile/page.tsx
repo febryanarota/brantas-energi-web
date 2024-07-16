282139ce5adb57cfd115b28cad063cba6635a838
febryanarota <arota0202@gmail.com>
2024-07-16 11:54:08 +0700

    [FIX] make profile cms page static

--- src/app/cms/(cms)/profile/page.tsx
+++ src/app/cms/(cms)/profile/page.tsx
@@ -4,44 +4,42 @@ import { profile } from "@prisma/client";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-async function getData(): Promise<profile[]> {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/profile`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Cookie: cookies().get("session")?.value || "",
-      },
-      credentials: "include",
-    });
+// async function getData(): Promise<profile[]> {
+//   try {
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/profile`, {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Cookie: cookies().get("session")?.value || "",
+//       },
+//       credentials: "include",
+//     });
 
-    if (!res.ok) {
-      throw new Error("Network response was not ok");
-    }
+//     if (!res.ok) {
+//       throw new Error("Network response was not ok");
+//     }
 
-    const result: profile[] = await res.json();
-    return result;
-  } catch (error) {
-    console.log(error);
-    return [];
-  }
-}
+//     const result: profile[] = await res.json();
+//     return result;
+//   } catch (error) {
+//     console.log(error);
+//     return [];
+//   }
+// }
 
 export default async function Page() {
   const session = await getSession();
   if (!session) redirect("/cms/login");
 
-  const data: profile[] = await getData();
+  // const data: profile[] = await getData();
   return (
     <div>
       <CMSContainer>
         <div className="flex flex-row justify-between items-center border-b-3 pb-2">
           <h1 className="text-3xl font-bold tracking-widerfle">
-            Company Profile
+            Profile
           </h1>
         </div>
-        <p>{data[0].facebook}</p>
-        <p>{data[0].instagram}</p>
       </CMSContainer>
     </div>
   );
