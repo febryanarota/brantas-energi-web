30f762c9472a66af772d445d0452f400c2c9123b
febryanarota <arota0202@gmail.com>
2024-07-16 11:54:30 +0700

    [FIX] fix href on sidebar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -10,6 +10,7 @@ export default function Sidebar() {
   const [isLoading, setIsLoading] = useState(false);
   const [isInformasiPublikOpen, setIsInformasiPulbikOpen] = useState(false);
   const [isLaporanLayananOpen, setIsLaporanLayananOpen] = useState(false);
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
   const router = useRouter();
 
   const handleTrigger = () => {
@@ -41,9 +42,15 @@ export default function Sidebar() {
     setIsLaporanLayananOpen(!isLaporanLayananOpen);
   };
 
+  const handleOpenProfile = () => {
+    closeAll();
+    setIsProfileOpen(!isProfileOpen);
+  }
+
   const closeAll = () => {
     setIsInformasiPulbikOpen(false);
     setIsLaporanLayananOpen(false);
+    setIsProfileOpen(false);
   };
 
   return (
@@ -54,12 +61,26 @@ export default function Sidebar() {
             <button onClick={handleTrigger} className="self-end px-5">
               <X />
             </button>
-            <a href="#" className="sidebar-item ">
+            <a href="/cms/home" className="sidebar-item ">
               Home
             </a>
-            <a href="#" className="sidebar-item">
-              Profil
+            <a className="sidebar-item flex flex-row justify-between" onClick={handleOpenProfile}>
+              Profile
+              {isProfileOpen ? <ChevronUp /> : <ChevronDown />}
             </a>
+            {isProfileOpen && (
+              <div className="flex flex-col">
+                <a href="/cms/struktur-ppid" className="sidebar-item-child">
+                  Struktur PPID
+                </a>
+                <a href="/cms/visi-misi-ppid" className="sidebar-item-child">
+                  Visi & Misi PPID
+                </a>
+                <a href="/cms/tugas-fungsi-wewenang" className="sidebar-item-child">
+                  Tugas, Fungsi, dan Wewenang
+                </a>
+              </div>
+            )}
             <a href="/cms/regulasi" className="sidebar-item">
               Regulasi
             </a>
@@ -72,16 +93,16 @@ export default function Sidebar() {
             </a>
             {isInformasiPublikOpen && (
               <div className="flex flex-col">
-                <a href="#" className="sidebar-item-child">
+                <a href="/cms/informasi-publik" className="sidebar-item-child">
                   Informasi Publik
                 </a>
-                <a href="#" className="sidebar-item-child">
+                <a href="/cms/informasi-serta-merta" className="sidebar-item-child">
                   Informasi Serta Merta
                 </a>
-                <a href="#" className="sidebar-item-child">
+                <a href="/cms/informasi-berkala" className="sidebar-item-child">
                   Informasi Berkala
                 </a>
-                <a href="#" className="sidebar-item-child">
+                <a href="/cms/informasi-wajib-tersedia" className="sidebar-item-child">
                   Informasi Wajib Tersedia
                 </a>
               </div>
@@ -95,27 +116,27 @@ export default function Sidebar() {
             </a>
             {isLaporanLayananOpen && (
               <div className="flex flex-col">
-                <a href="#" className="sidebar-item-child">
+                <a href="/cms/laporan-layanan-informasi" className="sidebar-item-child">
                   Laporan Layanan Informasi
                 </a>
-                <a href="#" className="sidebar-item-child">
+                <a href="/cms/permohonan-informasi" className="sidebar-item-child">
                   Permohonan Informasi
                 </a>
-                <a href="#" className="sidebar-item-child">
+                <a href="/cms/laporan-tahunan" className="sidebar-item-child">
                   Laporan Tahunan
                 </a>
-                <a href="#" className="sidebar-item-child">
+                <a href="/cms/laporan-berkelanjutan" className="sidebar-item-child">
                   Laporan Keberlanjutan
                 </a>
-                <a href="#" className="sidebar-item-child">
+                <a href="/cms/kepuasan-layanan-informasi-publik" className="sidebar-item-child">
                   Kepuasan Layanan Informasi Publik
                 </a>
               </div>
             )}
-            <a href="#" className="sidebar-item">
+            <a href="/cms/prosedur" className="sidebar-item">
               Prosedur
             </a>
-            <a href="#" className="sidebar-item">
+            <a href="/cms/pengaduan" className="sidebar-item">
               Pengaduan
             </a>
             <a href="/cms/faq" className="sidebar-item">
