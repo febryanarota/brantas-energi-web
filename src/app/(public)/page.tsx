dbed879145964e30a36d196a48e14da6899386d2
febryanarota <arota0202@gmail.com>
2024-07-18 15:37:34 +0700

    [FIX] integrate backend section 1 and section 2

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,110 +1,21 @@
-"use client";
+import Home from "@/components/home/home";
+import { home } from "@prisma/client";
 
-import Image from "next/image";
-import { EmblaOptionsType } from "embla-carousel";
-import Layanan from "@/components/carousel/layanan-carousel";
-import { Container } from "@/components/ui/container";
-
-export default function Home() {
-  const OPTIONS: EmblaOptionsType = {
-    loop: true,
-  };
-
-  const SLIDES = Array.from(Array(5).keys());
+async function getData() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/home`, {
+    method: "GET",
+  });
+  
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  
+  const data: { verified: home; pending: home } = await response.json();
+  return data.verified;
+}
+export default async function Page() {
+  const data = await getData();
   return (
-    <div className="flex flex-col">
-      {/* Landing image */}
-      <div className="w-full h-screen ">
-        <div className="relative w-full h-full">
-          <Image
-            src="/images/tol-cisumdawu.jpg"
-            width={2000}
-            height={1000}
-            alt=""
-            className="w-full h-full object-cover"
-          />
-          <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-10">
-            <Container className=" text-white sm:border-l-5 sm:border-primaryYellow py-10">
-              <div className="flex flex-col gap-4 border-b-5 border-primaryYellow pb-2 sm:border-none sm:pb-0">
-                <span className="text-4xl sm:text-6xl font-bold">
-                  Layanan PPID
-                </span>
-                <span className="text-4xl sm:text-6xl font-bold">
-                  PT Brantas Energi
-                </span>
-              </div>
-              <p className="py-5">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
-                ad nihil qui alias possimus ex! Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Cumque sapiente quos voluptates.
-                Lorem, ipsum.
-              </p>
-            </Container>
-          </div>
-        </div>
-      </div>
-
-      {/* Tentang Kami */}
-      <div className="flex flex-col items-center pb-24">
-        <Container className="flex flex-col sm:flex-row sm:mt-20 mt-10 items-center">
-          <div className="relative sm:w-1/2 h-fit">
-            <div className="relative w-[90%] h-[200px] sm:h-[350px] rounded-md overflow-hidden">
-              <Image
-                src="/images/tol-cisumdawu.jpg"
-                width={500}
-                height={500}
-                alt=""
-                className="w-full h-full object-cover mb-10"
-              />
-              <div className="absolute w-full h-full bg-primaryYellow inset-0 bg-opacity-30"></div>
-            </div>
-            <Image
-              src="/images/tol-cisumdawu.jpg"
-              width={500}
-              height={500}
-              alt=""
-              className="absolute top-0 w-[90%] right-0 z-10 translate-y-10 border-3 border-white rounded-md"
-            />
-          </div>
-
-          <div className="sm:w-1/2 sm:pl-10 flex flex-col">
-            <p className="border-b-2 border-primaryYellow font-semibold my-10 text-slate-700">
-              TENTANG KAMI
-            </p>
-            <p className="text-4xl font-semibold mb-5">
-              PPID PT BRANTAS ENERGI
-            </p>
-            <div className="flex flex-col gap-5">
-              <p className="text-justify">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Inventore, veniam? Ullam in ex nulla consectetur optio doloribus
-                mollitia ipsam recusandae, assumenda fuga natus nam! Numquam,
-                voluptatibus?
-              </p>
-              <p className="text-justify">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Inventore, veniam? Ullam in ex nulla consectetur optio doloribus
-                mollitia ipsam recusandae, assumenda fuga natus nam! Numquam,
-                voluptatibus?
-              </p>
-            </div>
-          </div>
-        </Container>
-      </div>
-
-      <div className="bg-gray-100 flex justify-center py-10">
-        <Container className="flex flex-col">
-          <div className="flex flex-col items-center">
-            <p className="border-b-2 border-primaryYellow font-semibold mt-10 text-slate-800">
-              LAYANAN KAMI
-            </p>
-            <p className="text-4xl font-semibold my-5 mb-10 md:text-left text-center">
-              Daftar Informasi Publik Brantas Energi
-            </p>
-          </div>
-          <Layanan slides={SLIDES} options={OPTIONS} />
-        </Container>
-      </div>
-    </div>
-  );
+    <Home data={data}/>
+  )
 }
---
+++ src/components/home/home.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import Image from "next/image";
+import { EmblaOptionsType } from "embla-carousel";
+import Layanan from "@/components/carousel/layanan-carousel";
+import { Container } from "@/components/ui/container";
+import { home } from "@prisma/client";
+
+export default function Home({ data }: { data: home }) {
+  const OPTIONS: EmblaOptionsType = {
+    loop: true,
+  };
+
+  const SLIDES = Array.from(Array(5).keys());
+  return (
+    <div className="flex flex-col">
+      {/* Landing image */}
+      <div className="w-full h-screen relative">
+        <div className="absolute inset-0 bg-black opacity-30"></div>
+        <Image
+          src={`${process.env.NEXT_PUBLIC_IMAGE_STORAGE_URL}/${data.image1}`}
+          width={2000}
+          height={1000}
+          alt=""
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 flex items-center justify-center">
+          <Container className="text-white sm:border-l-5 sm:border-primaryYellow py-10">
+            <div className="flex flex-col gap-4 border-b-5 border-primaryYellow pb-2 sm:border-none sm:pb-0">
+              <span className="text-4xl sm:text-6xl font-bold">
+                {data.heading1}
+              </span>
+            </div>
+            <div
+              className="ProseMirror whitespace-pre-line text-sm text-justify py-5"
+              style={{ whiteSpace: "pre-line" }}
+              dangerouslySetInnerHTML={{ __html: data.description1 as string }}
+            />
+          </Container>
+        </div>
+      </div>
+
+      {/* Tentang Kami */}
+      <div className="flex flex-col items-center pb-24">
+        <Container className="flex flex-col sm:flex-row sm:mt-20 mt-10 items-center">
+          <div className="relative sm:w-1/2 h-fit">
+            <div className="relative w-[90%] h-[200px] sm:h-[350px] rounded-md overflow-hidden">
+              <Image
+                src={`${process.env.NEXT_PUBLIC_IMAGE_STORAGE_URL}/${data.image2}`}
+                width={500}
+                height={500}
+                alt=""
+                className="w-full h-full object-cover mb-10"
+              />
+              <div className="absolute w-full h-full bg-primaryYellow inset-0 bg-opacity-30"></div>
+            </div>
+            <Image
+              src={`${process.env.NEXT_PUBLIC_IMAGE_STORAGE_URL}/${data.image2}`}
+              width={500}
+              height={500}
+              alt=""
+              className="absolute top-0 w-[90%] right-0 z-10 translate-y-10 border-3 border-white rounded-md"
+            />
+          </div>
+
+          <div className="sm:w-1/2 sm:pl-10 flex flex-col">
+            <p className="border-b-2 border-primaryYellow font-semibold my-10 text-slate-700">
+              {data.subheading2}
+            </p>
+            <p className="text-4xl font-semibold mb-5">{data.heading2}</p>
+            <div className="flex flex-col gap-5">
+              <div
+                className="ProseMirror whitespace-pre-line text-sm text-justify py-5"
+                style={{ whiteSpace: "pre-line" }}
+                dangerouslySetInnerHTML={{
+                  __html: data.description2 as string,
+                }}
+              />
+            </div>
+          </div>
+        </Container>
+      </div>
+
+      <div className="bg-gray-100 flex justify-center py-10">
+        <Container className="flex flex-col">
+          <div className="flex flex-col items-center">
+            <p className="border-b-2 border-primaryYellow font-semibold mt-10 text-slate-800">
+              LAYANAN KAMI
+            </p>
+            <p className="text-4xl font-semibold my-5 mb-10 md:text-left text-center">
+              Daftar Informasi Publik Brantas Energi
+            </p>
+          </div>
+          <Layanan slides={SLIDES} options={OPTIONS} />
+        </Container>
+      </div>
+    </div>
+  );
+}
