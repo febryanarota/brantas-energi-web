3d1e30db8ff97b7603ace39376226ba55ece69db
febryanarota <arota0202@gmail.com>
2024-07-22 11:11:01 +0700

    [FEAT] accept request home section 3 API

---
+++ src/app/api/home/section3/accept/route.ts
@@ -0,0 +1,72 @@
+import prisma from "@/lib/prisma";
+import storage from "@/lib/storage";
+import { subtractArrays } from "@/lib/utils";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PUT(req: NextRequest) {
+  // accept the request
+  const sessionExists = req.cookies.get("session");
+
+  if (!sessionExists) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  try {
+    const verified = await prisma.home.findFirst({
+      where: {
+        status: "verified",
+      },
+    });
+
+    const pending = await prisma.home.findFirst({
+      where: {
+        status: "updatePending",
+      },
+    });
+
+    // remove = verified cards - pending cards
+    const removeCard = subtractArrays(
+      verified?.cards as number[],
+      pending?.cards as number[],
+    );
+
+    // remove the image cards in storage
+    removeCard.map(async (card) => {
+      let deleteCard = await prisma.card.findFirst({
+        where: {
+          id: card,
+        },
+      });
+
+      let path = "public/card/" + (deleteCard?.image as string).split("/").pop();
+      storage.image.delete(path).catch((error) => {
+        console.error("Error deleting image:", error);
+        throw new Error("Error deleting image");
+      });
+      // delete the cards instance
+      await prisma.card.delete({
+        where: {
+          id: card,
+        },
+      });
+    });
+
+    // update the verified data = pending data
+    const result = await prisma.home.updateMany({
+      where: {
+        status: "verified",
+      },
+      data: {
+        heading3: pending?.heading3,
+        subheading3: pending?.subheading3,
+        cards: pending?.cards,
+      },
+    });
+
+    return NextResponse.json(result);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error updating section 1" },
+      { status: 500 },
+    );
+  }
+}
--- src/app/api/home/section3/route.ts
+++ src/app/api/home/section3/route.ts
@@ -1,71 +1,64 @@
+import { decrypt } from "@/lib/auth";
 import prisma from "@/lib/prisma";
-import storage from "@/lib/storage";
-import { subtractArrays } from "@/lib/utils";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function PUT(req: NextRequest) {
-  // accept the request
+export async function PATCH(req: NextRequest) {
+  // handle session
   const sessionExists = req.cookies.get("session");
 
   if (!sessionExists) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
-  try {
-    const verified = await prisma.home.findFirst({
-      where: {
-        status: "verified",
-      },
-    });
 
-    const pending = await prisma.home.findFirst({
+  const body = await req.formData();
+  if (!body) {
+    return NextResponse.json({ error: "No file provided" }, { status: 400 });
+  }
+
+  const session = await decrypt(sessionExists.value);
+  const role = session.role;
+
+  const heading = body.get("heading3") as string;
+  const subheading = body.get("subheading3") as string;
+
+  if (!heading || !subheading) {
+    return NextResponse.json(
+      { error: "Missing required fields" },
+      { status: 400 },
+    );
+  }
+
+  try {
+    // admin -> update both verified and pending
+    // user -> update pending
+    const updatePending = await prisma.home.updateMany({
       where: {
         status: "updatePending",
       },
-    });
-
-    // remove = verified cards - pending cards
-    const removeCard = subtractArrays(
-      verified?.cards as number[],
-      pending?.cards as number[],
-    );
-
-    // remove the image cards in storage
-    removeCard.map(async (card) => {
-      let deleteCard = await prisma.card.findFirst({
-        where: {
-          id: card,
-        },
-      });
-
-      let path = "public/card/" + (deleteCard?.image as string).split("/").pop();
-      storage.image.delete(path).catch((error) => {
-        console.error("Error deleting image:", error);
-        throw new Error("Error deleting image");
-      });
-      // delete the cards instance
-      await prisma.card.delete({
-        where: {
-          id: card,
-        },
-      });
-    });
-
-    // update the verified data = pending data
-    const result = await prisma.home.updateMany({
-      where: {
-        status: "verified",
-      },
       data: {
-        heading3: pending?.heading3,
-        subheading3: pending?.subheading3,
-        cards: pending?.cards,
+        heading3: heading,
+        subheading3: subheading,
       },
     });
 
-    return NextResponse.json(result);
+    if (role === "admin") {
+      const updateVerified = await prisma.home.updateMany({
+        where: {
+          status: "verified",
+        },
+        data: {
+          heading3: heading,
+          subheading3: subheading,
+        },
+      });
+
+      return NextResponse.json({ updatePending, updateVerified });
+    }
+
+    // return NextResponse.json(update);
   } catch (error) {
     return NextResponse.json(
-      { error: "Error updating section 1" },
+      { error: "Error acception request section 1" },
       { status: 500 },
     );
   }
