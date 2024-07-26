887ea91be61903181862b9e7407c6629f81de335
febryanarota <arota0202@gmail.com>
2024-07-26 09:21:27 +0700

    [FEAT] handle delete file image by admin

--- src/app/api/fileImageBuffer/[id]/route.ts
+++ src/app/api/fileImageBuffer/[id]/route.ts
@@ -28,31 +28,79 @@ export async function DELETE(
 
   const body = await req.json();
   const id = context.params.id;
-  const editId = body.editId;
 
-  const oldBufferRes = await prisma.fileImageBuffer.delete({
-    where: {
-      id: parseInt(id),
-    },
-  });
+  if (body.editId) {
+    const editId = body.editId;
 
-  const newBufferRes = await prisma.fileImageBuffer.findFirst({
-    where: {
-      id: editId,
-    }
-  })
-    
+    const oldBufferRes = await prisma.fileImageBuffer.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
 
-  const oldIds = oldBufferRes.fileImageIds;
-  const newIds = newBufferRes?.fileImageIds;
+    const newBufferRes = await prisma.fileImageBuffer.findFirst({
+      where: {
+        id: editId,
+      }
+    })
+      
 
-  // deleteIds = oldIds - newIds
-  const deleteIds = oldIds.filter((id) => !newIds?.includes(id));
-  console.log(deleteIds);
-  if (deleteIds.length > 0) {
-    // loop through deleteIds and delete the image and file in storage
-    deleteIds.forEach(async (id) => {
-      console.log(id); 
+    const oldIds = oldBufferRes.fileImageIds;
+    const newIds = newBufferRes?.fileImageIds;
+
+    // deleteIds = oldIds - newIds
+    const deleteIds = oldIds.filter((id) => !newIds?.includes(id));
+    console.log(deleteIds);
+    if (deleteIds.length > 0) {
+      // loop through deleteIds and delete the image and file in storage
+      deleteIds.forEach(async (id) => {
+        console.log(id); 
+        const fileImage = await prisma.fileImage.findFirst({
+          where: {
+            id: id,
+          },
+        });
+
+        if (fileImage) {
+          if (fileImage.link && fileImage.isFile) {
+            const file = "public/file-image/" + fileImage.link.split("/").pop();
+            await storage.file.delete(file as string).catch((error) => {
+              console.error("Error deleting file:", error);
+              throw new Error("Error deleting file");
+            });
+          } 
+
+          if (fileImage.image) {
+            const image = "public/file-image/" + fileImage.image.split("/").pop();
+            await storage.image.delete(image as string).catch((error) => {
+              console.error("Error deleting image:", error);
+              throw new Error("Error deleting image");
+            });
+          }
+        }
+
+        await prisma.fileImage.delete({
+          where: {
+            id: id,
+          },
+        });
+
+
+      });
+    };
+
+    //return newBuffer
+    return NextResponse.json(newBufferRes);
+  } else {
+    const fileImage = await prisma.fileImageBuffer.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    const fileImageIds = fileImage.fileImageIds;
+    // loop through fileImageIds and delete the image and file in storage
+    fileImageIds.forEach(async (id) => {
       const fileImage = await prisma.fileImage.findFirst({
         where: {
           id: id,
@@ -82,11 +130,8 @@ export async function DELETE(
           id: id,
         },
       });
-
-
     });
-  };
 
-  //return newBuffer
-  return NextResponse.json(newBufferRes);
+    return NextResponse.json(fileImage);
+  }
 }
