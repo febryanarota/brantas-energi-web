dd4ff105f69e0aa44b819a99f2818bcb9807c00d
febryanarota <arota0202@gmail.com>
2024-08-05 10:18:16 +0700

    [FIX] favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-import NavigationBar from "@/components/navbar/navbar";
-import Footer from "@/components/footer/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Home | PPID Brantas Energi",
+  icons: {
+    icon: '/images/logo.png', 
+  },
 };
 
 export default function RootLayout({
