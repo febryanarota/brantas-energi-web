cebc291f66beade4c7957b879dd8e56f958a108a
febryanarota <arota0202@gmail.com>
2024-08-20 09:51:46 +0700

    [FIX] handle session for form pages

--- src/app/cms/(cms)/form-kepuasan-layanan/page.tsx
+++ src/app/cms/(cms)/form-kepuasan-layanan/page.tsx
@@ -1,7 +1,12 @@
 import { CMSContainer } from "@/components/ui/container";
 import TableComponent from "./kepuasan-table";
+import { getSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
+  const session = await getSession();
+  if (!session) redirect("/cms/login");
+  
   return (
     <div className="w-full">
       <CMSContainer>
